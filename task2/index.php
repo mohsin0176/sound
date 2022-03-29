a1f35c5e85d8cbc160ddacfc76d587eb7311d46a
MD.MOHSIN <74142229+mohsin0176@users.noreply.github.com>
2022-03-29 23:53:21 +0600

    task 2 complete

---
+++ task2/index.php
@@ -0,0 +1,73 @@
+<?php
+
+$b="CASE:";
+
+$a=$_GET['command'];
+
+$o=ltrim($a,"OPEN");
+$dir=array("C:\");
+
+$new_dir=array();
+
+if( is_int($a) && ($a<=60) )
+
+{
+
+echo $b.$a;
+
+}
+
+if($o)
+
+{
+    
+array_push($dir,$o);
+    
+$new_dir=$dir;
+    
+echo $o;
+
+}
+
+if($a == "QUIT")
+
+{
+
+echo "<script> window.close();</script>";
+
+}
+
+
+if($a == "BACK")
+
+{ 
+
+if(count($dir)>0)
+
+{
+    
+array_pop($dir);
+    
+$l=count($dir);
+    
+$ln=$l-1;
+    
+echo $dir[$ln];
+
+}
+
+else
+
+{
+    
+echo "Ignored";
+
+}
+    
+
+
+}
+
+
+
+?>
